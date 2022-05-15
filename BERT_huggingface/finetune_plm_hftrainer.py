b6a2c13c8aabbd195abfa963b1b333baaff80bae
codingchild <codingchild@gmail.com>
2022-05-15 11:35:00 +0000

    change finetune_plmj_hftrainer.py

--- BERT_huggingface/finetune_plm_hftrainer.py
+++ BERT_huggingface/finetune_plm_hftrainer.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import accuracy_score
+import random
 
 import torch
 
@@ -9,8 +10,80 @@ from transformers import TrainingArguments
 
 #이것들만 구현해서 모듈을 불러온 것
 from bert_data_loader import TextClassificationCollator, TextClassificationDataset
-from utils import get_datasets
-from define_argparser import define_argparser
+
+import argparse
+
+def define_argparser():
+    p = argparse.ArgumentParser()
+
+    p.add_argument('--model_fn', required=True)
+    p.add_argument('--train_fn', required=True)
+    # Recommended model list:
+    # - kykim/bert-kor-base
+    # - kykim/albert-kor-base
+    # - beomi/kcbert-base
+    # - beomi/kcbert-large
+    p.add_argument('--pretrained_model_name', type=str, default='beomi/kcbert-base')
+    p.add_argument('--use_albert', action='store_true')
+
+    p.add_argument('--valid_ratio', type=float, default=.2)
+    #멀티 GPU라서 이렇게 함, GPU가 1개면 그냥 돌려도 됨
+    p.add_argument('--batch_size_per_device', type=int, default=32)
+    p.add_argument('--n_epochs', type=int, default=5)
+
+    p.add_argument('--warmup_ratio', type=float, default=.2)
+
+    p.add_argument('--max_length', type=int, default=100)
+
+    config = p.parse_args()
+
+    return config
+
+def read_text(fn):
+    with open(fn, 'r') as f:
+        lines = f.readlines()
+
+        labels, texts = [], []
+        for line in lines:
+            if line.strip() != '':
+                # The file should have tab delimited two columns.
+                # First column indicates label field,
+                # and second column indicates text field.
+                label, text = line.strip().split('\t') #tsv 파일, 첫번째 column이 label, 두번째가 text
+                labels += [label]
+                texts += [text]
+
+    return labels, texts
+
+def get_datasets(
+    fn, #config.train_fn 값이 들어감
+    valid_ratio=.2
+    ):
+     # Get list of labels and list of texts.
+    labels, texts = read_text(fn)
+
+    # Generate label to index map.
+    unique_labels = list(set(labels))
+    label_to_index = {}
+    index_to_label = {}
+    for i, label in enumerate(unique_labels):
+        label_to_index[label] = i
+        index_to_label[i] = label
+
+    # Convert label text to integer value.
+    labels = list(map(label_to_index.get, labels))
+
+    # Shuffle before split into train and validation set.
+    shuffled = list(zip(texts, labels))
+    random.shuffle(shuffled)
+    texts = [e[0] for e in shuffled]
+    labels = [e[1] for e in shuffled]
+    idx = int(len(texts) * (1 - valid_ratio))
+
+    train_dataset = TextClassificationDataset(texts[:idx], labels[:idx])
+    valid_dataset = TextClassificationDataset(texts[idx:], labels[idx:])
+
+    return train_dataset, valid_dataset, index_to_label
 
 
 def main(config):
--- BERT_huggingface/utils.py
+++ BERT_huggingface/utils.py
@@ -1,24 +1,3 @@
-import random
-from bert_data_loader import TextClassificationCollator, TextClassificationDataset
-
-#파일 읽어서 열어줌
-def read_text(fn):
-    with open(fn, 'r') as f:
-        lines = f.readlines()
-
-        labels, texts = [], []
-        for line in lines:
-            if line.strip() != '':
-                # The file should have tab delimited two columns.
-                # First column indicates label field,
-                # and second column indicates text field.
-                label, text = line.strip().split('\t') #tsv 파일, 첫번째 column이 label, 두번째가 text
-                labels += [label]
-                texts += [text]
-
-    return labels, texts
-
-
 def get_grad_norm(parameters, norm_type=2):
     parameters = list(filter(lambda p: p.grad is not None, parameters))
 
@@ -47,32 +26,3 @@ def get_parameter_norm(parameters, norm_type=2):
     return total_norm
 
 
-def get_datasets(
-    fn, #config.train_fn 값이 들어감
-    valid_ratio=.2
-    ):
-     # Get list of labels and list of texts.
-    labels, texts = read_text(fn)
-
-    # Generate label to index map.
-    unique_labels = list(set(labels))
-    label_to_index = {}
-    index_to_label = {}
-    for i, label in enumerate(unique_labels):
-        label_to_index[label] = i
-        index_to_label[i] = label
-
-    # Convert label text to integer value.
-    labels = list(map(label_to_index.get, labels))
-
-    # Shuffle before split into train and validation set.
-    shuffled = list(zip(texts, labels))
-    random.shuffle(shuffled)
-    texts = [e[0] for e in shuffled]
-    labels = [e[1] for e in shuffled]
-    idx = int(len(texts) * (1 - valid_ratio))
-
-    train_dataset = TextClassificationDataset(texts[:idx], labels[:idx])
-    valid_dataset = TextClassificationDataset(texts[idx:], labels[idx:])
-
-    return train_dataset, valid_dataset, index_to_label
